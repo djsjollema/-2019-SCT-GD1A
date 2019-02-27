fda5688681474062fc6eac58f8778e9eb610e5fb
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-02-27 12:04:59 +0100

    02-03

--- html/02blocz/script.js
+++ html/02blocz/script.js
@@ -1,1 +1,5 @@
-console.log("hallo wereld");
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
