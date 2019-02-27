2b76570eb481152684cffc62ab177388b50f1f5c
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-02-27 12:24:39 +0100

    02_04

--- html/02blocz/script.js
+++ html/02blocz/script.js
@@ -1,11 +1,27 @@
+class Bloc {
+  constructor(x1,y1,x2,y2) {
+    this.x1= x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  draw(){
+    context.beginPath();
+    context.fillStyle = "lightblue";
+    context.strokeStyle = "blue";
+    context.rect(this.x1,this.y1,this.x2,this.y2);
+    context.closePath();
+    context.stroke();
+    context.fill();
+  }
+}
+
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-context.beginPath();
-context.rect(10,10,100,100);
-context.closePath();
-context.stroke();
-context.fill();
+let myBlok = new Bloc(0,0,200,300);
+myBlok.draw();
