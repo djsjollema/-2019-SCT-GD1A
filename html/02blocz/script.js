e4590ed757fa6ff39715a894b39e3e814c5c6d0f
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-02-27 12:10:32 +0100

    02_03

--- html/02blocz/script.js
+++ html/02blocz/script.js
@@ -3,3 +3,9 @@ const context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+context.beginPath();
+context.rect(10,10,100,100);
+context.closePath();
+context.stroke();
+context.fill();
