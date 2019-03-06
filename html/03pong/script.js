e49ac65d13144e71fa006e36ab7ecc330a6c2661
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-06 13:25:00 +0100

    03_3

--- html/03pong/script.js
+++ html/03pong/script.js
@@ -1,12 +1,14 @@
 class Ball {
-  constructor() {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
 
   }
 
   draw(context){
     context.beginPath();
     context.fillStyle = "rgba(255,255,0,0.5)"
-    context.arc(100,100,30,0,Math.PI*2);
+    context.arc(this.x,this.y,30,0,Math.PI*2);
     context.closePath();
     context.stroke();
     context.fill();
@@ -16,5 +18,18 @@ class Ball {
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-let myBall = new Ball();
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let myBall = new Ball(100,100);
 myBall.draw(context);
+
+
+function animate(){
+  window.requestAnimationFrame(animate);
+  //console.log(positionX);
+  myBall.draw(context);
+  myBall.x = myBall.x + 1;
+}
+
+animate();
