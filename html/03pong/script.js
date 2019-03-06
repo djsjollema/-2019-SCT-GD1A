7c275334f0eb9d399a0fc371c709783adf2927c4
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-06 12:13:40 +0100

    03_2

--- html/03pong/script.js
+++ html/03pong/script.js
@@ -1,1 +1,20 @@
-console.log('hallo wereld');
+class Ball {
+  constructor() {
+
+  }
+
+  draw(context){
+    context.beginPath();
+    context.fillStyle = "rgba(255,255,0,0.5)"
+    context.arc(100,100,30,0,Math.PI*2);
+    context.closePath();
+    context.stroke();
+    context.fill();
+  }
+}
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+let myBall = new Ball();
+myBall.draw(context);
