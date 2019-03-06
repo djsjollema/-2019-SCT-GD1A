dd7cca425fd06e4a097043e568fef6f7589f4ff8
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-06 13:44:47 +0100

    03_5

--- html/03pong/script.js
+++ html/03pong/script.js
@@ -7,7 +7,7 @@ class Ball {
 
   draw(context){
     context.beginPath();
-    context.fillStyle = "rgba(255,255,0,0.5)"
+    context.fillStyle = "rgba(255,255,0,1)"
     context.arc(this.x,this.y,30,0,Math.PI*2);
     context.closePath();
     context.stroke();
@@ -24,17 +24,28 @@ canvas.height = window.innerHeight;
 let myBall = new Ball(100,100);
 myBall.draw(context);
 
-let speedX = 2;
-let speedY = 3;
+let speedX = 20;
+let speedY = 40;
 
 
 function animate(){
   window.requestAnimationFrame(animate);
   context.clearRect(0,0,canvas.width,canvas.height);
-  //console.log(positionX);
   myBall.draw(context);
   myBall.x += speedX;
   myBall.y += speedY;
+  if(myBall.y > canvas.height-30){
+    speedY = -speedY;
+  }
+  if(myBall.y <0){
+    speedY = -speedY;
+  }
+  if(myBall.x > canvas.width - 30){
+    speedX = -speedX;
+  }
+  if(myBall.x < 0){
+    speedX = -speedX;
+  }
 }
 
 animate();
