1807faef9bcec2e04524ae09c1093aa6c82613c4
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-06 14:27:17 +0100

    03_6

--- html/03pong/script.js
+++ html/03pong/script.js
@@ -2,12 +2,11 @@ class Ball {
   constructor(x,y) {
     this.x = x;
     this.y = y;
-
   }
 
   draw(context){
     context.beginPath();
-    context.fillStyle = "rgba(255,255,0,1)"
+    context.fillStyle = "rgba(0,0,255,1)"
     context.arc(this.x,this.y,30,0,Math.PI*2);
     context.closePath();
     context.stroke();
@@ -24,9 +23,14 @@ canvas.height = window.innerHeight;
 let myBall = new Ball(100,100);
 myBall.draw(context);
 
-let speedX = 20;
-let speedY = 40;
+let myBall2 = new Ball(400,500);
+myBall2.draw(context);
 
+let speedX = 2;
+let speedY = 4;
+
+let speedX2 = 4;
+let speedY2 = -3;
 
 function animate(){
   window.requestAnimationFrame(animate);
@@ -34,17 +38,36 @@ function animate(){
   myBall.draw(context);
   myBall.x += speedX;
   myBall.y += speedY;
+
+
+  myBall2.draw(context);
+  myBall2.x += speedX2;
+  myBall2.y += speedY2;
+
   if(myBall.y > canvas.height-30){
     speedY = -speedY;
   }
-  if(myBall.y <0){
+  if(myBall.y < 30){
     speedY = -speedY;
   }
   if(myBall.x > canvas.width - 30){
     speedX = -speedX;
   }
-  if(myBall.x < 0){
+  if(myBall.x < 30){
     speedX = -speedX;
+  }
+
+  if(myBall2.y > canvas.height-30){
+    speedY2 = -speedY2;
+  }
+  if(myBall2.y < 30){
+    speedY2 = -speedY2;
+  }
+  if(myBall2.x > canvas.width - 30){
+    speedX2 = -speedX2;
+  }
+  if(myBall2.x < 30){
+    speedX2 = -speedX2;
   }
 }
 
