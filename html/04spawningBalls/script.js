160755800f2c59e0c5a98086fd0f8f8bd4d42399
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-20 12:21:38 +0100

    02_4

--- html/04spawningBalls/script.js
+++ html/04spawningBalls/script.js
@@ -3,6 +3,8 @@ class Ball {
     this.x = x;
     this.y = y;
     this.r = r;
+    this.v_x = 1;
+    this.v_y = 1;
     this.color = color;
   }
 
@@ -23,16 +25,33 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let myBall = new Ball(getRandomNumber(canvas.width),getRandomNumber(canvas.height),20,"yellow");
-myBall.draw(context);
+let balls = [];
 
-// function animate(){
-//   requestAnimationFrame(animate);
-//   context.clearRect(0,0,canvas.width,canvas.height);
-//   myBall.draw(context);
-// }
+for(let i= 0; i<100; i++){
+  let myBall = new Ball(getRandomNumber(canvas.width),getRandomNumber(canvas.height),5,"yellow");
+  balls.push(myBall);
+  myBall.draw(context);
+}
 
-//animate();
+
+
+function animate(){
+  requestAnimationFrame(animate);
+  context.clearRect(0,0,canvas.width,canvas.height);
+  for(let i=0; i<balls.length; i++){
+    let myBall = balls[i];
+    myBall.x += myBall.v_x;
+    myBall.y += myBall.v_y;
+    if(myBall.x <0 || myBall.x > canvas.width){
+      myBall.v_x = - myBall.v_x;
+    }
+    if(myBall.y <0 || myBall.y > canvas.height){
+      myBall.v_y = - myBall.v_y;
+    }
+    myBall.draw(context);
+  }
+}
+animate();
 
 function getRandomNumber(max){
   return Math.floor(Math.random()*max);
