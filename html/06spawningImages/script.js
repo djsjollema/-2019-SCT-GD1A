371d3c116643911b4e7377e525a4ba446c528af4
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-27 12:24:55 +0100

    06_02

--- html/06spawningImages/script.js
+++ html/06spawningImages/script.js
@@ -3,7 +3,21 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let image = new Image();
+image.src = "apple.png";
 
+image.addEventListener('load',()=>{
+  animate();
+})
+
+
+function animate(){
+
+  requestAnimationFrame(animate);
+  if(Math.random() < 0.1){
+    context.drawImage(image,getRandomNumber(canvas.width),getRandomNumber(canvas.height));
+  }
+}
 
 function getRandomNumber(max){
   return Math.floor(Math.random()*max);
