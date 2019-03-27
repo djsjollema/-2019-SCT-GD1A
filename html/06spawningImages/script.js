b4610b18b76e2a313c1933e50ef6fd7aff76aefe
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-27 13:30:59 +0100

    06_03

--- html/06spawningImages/script.js
+++ html/06spawningImages/script.js
@@ -3,19 +3,26 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let image = new Image();
-image.src = "apple.png";
+let gameObjects = [];
 
-image.addEventListener('load',()=>{
-  animate();
-})
-
+animate();
 
 function animate(){
-
+  if(Math.random()<0.05){
+    let gameObject = {};
+    gameObject.image = new Image();
+    gameObject.image.src = "apple.png";
+    gameObject.x = getRandomNumber(canvas.width);
+    gameObject.y = -150;
+    gameObject.v_y = 5;
+    gameObjects.push(gameObject);
+  }
+  context.clearRect(0,0,canvas.width,canvas.height);
   requestAnimationFrame(animate);
-  if(Math.random() < 0.1){
-    context.drawImage(image,getRandomNumber(canvas.width),getRandomNumber(canvas.height));
+  for(let i = 0; i< gameObjects.length;i++){
+    let gameObject = gameObjects[i];
+    gameObject.y += gameObject.v_y;
+    context.drawImage(gameObject.image,gameObject.x,gameObject.y);
   }
 }
 
