c70870809963702b43187565a12d98cc94cd46d0
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-27 12:11:42 +0100

    06_01

---
+++ html/06spawningImages/script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+
+function getRandomNumber(max){
+  return Math.floor(Math.random()*max);
+}
