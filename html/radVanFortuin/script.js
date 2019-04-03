1c6a709b1d1af047e20f25899c851699ad907e76
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-04-03 13:24:58 +0200

    wheelOfFortune_2

--- html/radVanFortuin/script.js
+++ html/radVanFortuin/script.js
@@ -8,8 +8,11 @@ let wheel = new Image();
 wheel.src = "rad_van_fortuin.png";
 
 let myRotation = 0;
+let speed =0;
 
 wheel.addEventListener('load',()=>{
+  wheel.width = 500;
+  wheel.height = 500;
   animate();
 
 })
@@ -17,9 +20,18 @@ wheel.addEventListener('load',()=>{
 function animate(){
   context.clearRect(0,0,canvas.width,canvas.height);
   requestAnimationFrame(animate);
-  context.translate(500,500);
+  context.translate(canvas.width/2,canvas.height/2);
   context.rotate(myRotation);
-  context.drawImage(wheel,-250,-250,500,500);
+
+  context.drawImage(wheel,-wheel.width/2,-wheel.height/2,wheel.width,wheel.height);
   context.resetTransform();
-  myRotation += 0.01;
+  myRotation += speed;
+  speed *= 0.99;
+  if(speed<0.01){
+    speed = 0;
+  }
 }
+
+addEventListener('mousedown',()=>{
+  speed = Math.random();
+})
